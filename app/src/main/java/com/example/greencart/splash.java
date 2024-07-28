d30685618d0d62d5e14e4ab425d0522baf37c2b9
Maitreya Mishra <maitreyamishra04@gmail.com>
2024-07-28 18:24:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/greencart/splash.java
@@ -0,0 +1,34 @@
+package com.example.greencart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.splash_activity);
+
+        ImageView splashImageView = findViewById(R.id.splashImageView);
+
+        splashImageView.postDelayed(() -> {
+            Intent intent = new Intent(splash.this, Onboarding.class);
+            startActivity(intent);
+            finish();
+        }, 3000); // Display the splash screen for 3 seconds
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
